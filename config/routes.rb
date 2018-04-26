1c09f24a93d0485f13612afbae8a20f94506f57f
saxgutierrez <diegofdog@gmail.com>
2018-04-26 12:03:40 -0500

    arreglando problema calificador

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
     #namespace 'api' do
     namespace :api, defaults: { format: "json" } do
-    namespace 'v1' do
-    resources :products
+    namespace :v1 do
+    resources :products, only: [:index]
     end
 end
 end
