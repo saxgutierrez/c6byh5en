3217a553259b690e7f0fb964b0a2b8e1c1876668
saxgutierrez <diegofdog@gmail.com>
2018-05-04 15:58:00 -0500

    reto 3 delete and update

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -22,18 +22,24 @@ module Api
               end
         	end
 
-        	def edit
-        	  @product = Product.find(params[:id])
-        	end
+        	#def edit
+        	 # @product = Product.find(params[:id])
+        	#end
 
         	def update
-        	  @product = Product.update(params[:id], product_params)
+              @product = Product.find(params[:id])
+        	  if @product.update(product_params)
+              render json: @product, status: 200
+              else
+              render json: { errors: @product.errors }, status: 422
+              end
         	end
 
             def destroy
-              @product = Product.find(params[:id])
-              @product.destroy
-              head :no_content
+               @product = Product.find(params[:id])
+               @product.destroy
+               render json: @product, status: 204
+               head :no_content
             end
 
         	private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     #namespace 'api' do
     namespace :api, defaults: { format: :json } do
     namespace :v1 do
-    resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :products, only: [:index, :new, :create, :update, :destroy]
     end
 end
 end
